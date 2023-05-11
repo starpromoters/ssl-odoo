aa48a2f5d95a0aa4433607612d98fa02c008c377
Sathish-0901 <sathishkmr12345@gmail.com>
2023-05-11 13:53:00 +0530

    moved the po ref label change based on company wise code

--- star_extensions/models/sale_order.py
+++ star_extensions/models/sale_order.py
@@ -3,6 +3,8 @@ from odoo import api, fields, models, SUPERUSER_ID, _
 import logging
 _logger = logging.getLogger(__name__)
 from odoo.exceptions import AccessError, UserError, ValidationError
+from lxml import etree
+
 
 
 
@@ -11,6 +13,17 @@ class SaleOrder(models.Model):
 
     contact_name = fields.Char(string='Contact Name')
     p_o_ref = fields.Char(string='Purchase Order Ref')
+
+    @api.model
+    def fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
+        res = super(SaleOrder, self).fields_view_get(view_id=view_id, view_type=view_type, toolbar=toolbar, submenu=submenu)
+        if view_type == 'form':
+            doc = etree.XML(res['arch'])
+            for node in doc.xpath("//field[@name='p_o_ref']"):
+                if self.env.company.id == 7:
+                    node.set('string', 'AR')
+            res['arch'] = etree.tostring(doc, encoding='unicode')
+        return res
     
     def action_confirm(self):
         if not self.p_o_ref:
@@ -23,5 +36,7 @@ class SaleOrder(models.Model):
         invoice_vals = super(SaleOrder, self)._prepare_invoice()
         invoice_vals['p_o_ref'] = self.p_o_ref
         return invoice_vals
+
+
 
 
