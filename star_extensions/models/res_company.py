de89cd69b7fda09920470c416a116d2484de4c45
Aarthi Siva <aarthi.siva@apagen.com>
2023-06-02 19:30:37 +0530

    Aarthi- Html field added at terms and condition under companies and updated in reports

--- star_extensions/models/res_company.py
+++ star_extensions/models/res_company.py
@@ -6,5 +6,6 @@ _logger = logging.getLogger(__name__)
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
-    sale_terms_cond = fields.Text(string='Sale Terms & Condition')
-    invoice_terms_cond = fields.Text(string='Invoice Terms & Condition')
+    sale_terms_cond = fields.Html(string='Sale Terms & Condition')
+    invoice_terms_cond = fields.Html(string='Invoice Terms & Condition')
+    
