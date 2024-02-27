4a2ad46f0cee9f1b6adb1493fc1c58c893c96651
samm-ltd <saamodoo@gmail.com>
2024-02-27 16:10:24 +0530

    Aarthi - Company modified

--- star_extensions/models/product.py
+++ star_extensions/models/product.py
@@ -9,4 +9,5 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     hs_code = fields.Char(string='HS Code',copy=False)
+    company_id = fields.Many2one('res.company', 'Company', index=1, default=lambda self: self.env.company)
    
