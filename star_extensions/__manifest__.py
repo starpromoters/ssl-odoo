dee22d86717cddda051ef9d734320ea186c49dd5
vishal <vishal>
2026-09-24 14:22:01 +0530

    [FIX]

--- star_extensions/__manifest__.py
+++ star_extensions/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Star-Extensions',
-    'version': '19.0.1.0.0',
+    'version': '19.0.1.0.1',
     'category': '',
     'summary': 'Star-Extensions',
     
--- star_reports/__manifest__.py
+++ star_reports/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Star-Reports',
-    'version': '19.0.1.0.0',
+    'version': '19.0.1.0.1',
     'category': '',
     'summary': 'Star-Reports',
     
